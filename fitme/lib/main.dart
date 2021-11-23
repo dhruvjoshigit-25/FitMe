bdd2c2150450536f99d1c56be4f6a48ffd18b111
unknown <jaybhanushali532@gmail.com>
2021-11-23 14:27:24 +0530

    Initial change to main.dart

--- fitme/lib/main.dart
+++ fitme/lib/main.dart
@@ -20,7 +20,7 @@ class NewHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text("App bar for MyApp")),
+        title: Center(child: Text("FITMe", textAlign: TextAlign.center,)),
       ),
       body: Center(
         child: Container(
@@ -31,25 +31,146 @@ class NewHomePage extends StatelessWidget {
               Container(
                 padding: const EdgeInsets.all(8),
 
-                width: 150,
-                height: 100,
-                child: Text("FITMe",
-                  style: TextStyle(fontSize: 40,
+
+
+                child: Text("WELCOME, User",
+                  style: TextStyle(fontSize: 50,
                       fontWeight: FontWeight.bold),
                 ),
 
 
               ),
               Container(
-                padding: const EdgeInsets.all(8),
-
+                padding: const EdgeInsets.all(0),
+                margin: const EdgeInsets.only(top: 20.0),
                 width: 150,
-                height: 100,
-                child: Text("FITMe",
-                  style: TextStyle(fontSize: 40,
+
+                child: Text("Login",
+                  style: TextStyle(fontSize: 25,
+                      fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+
+
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+
+                width: 300,
+                child: TextField(
+
+                  decoration: InputDecoration(
+
+                      border: OutlineInputBorder(),
+                      labelText: ("Email"),
+                      hintText: 'Enter valid mail id'
+                  ),
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                width: 300,
+
+                child: TextField(
+
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+
+                  ),
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+
+
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 20.0),
+
+                height: 50,
+                width: 250,
+                decoration: BoxDecoration(
+                    color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (_) => NewHomePage()));
+                  },
+                  child: Text(
+                    'Login',
+                    style: TextStyle(color: Colors.black, fontSize: 25),
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                margin: const EdgeInsets.only(top: 15.0),
+
+                child: Text("------------------OR------------------",
+                  style: TextStyle(fontSize: 20,
                       fontWeight: FontWeight.bold),
                 ),
 
+
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 30.0),
+                height: 40,
+                width: 250,
+                decoration: BoxDecoration(
+                    color: Colors.blue[200], borderRadius: BorderRadius.circular(20)),
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (_) => NewHomePage()));
+                  },
+                  child: Text(
+                    'Continue with Google',
+                    style: TextStyle(color: Colors.black, fontSize: 20),
+                  ),
+                ),
+              ),
+
+
+
+              Container(
+                margin: const EdgeInsets.only(top: 20.0),
+                child: Row(
+
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+
+
+                      child : Text("Not A Member?",
+                        style: TextStyle(fontSize: 15,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Container(
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context, MaterialPageRoute(builder: (_) => NewHomePage()));
+                          },
+
+                          child : Text(" Sign Up!",
+                            style: TextStyle(fontSize: 15,
+                                fontWeight: FontWeight.bold, color: Colors.blue),
+                          ),
+                        )
+                    )
+                  ],
+
+                ),
 
               ),
 
@@ -102,12 +223,7 @@ class NewHomePage extends StatelessWidget {
 
 
 
-      // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        onPressed:(){} ,
-        child: Icon(Icons.edit),
 
-      ),
 
 
 
